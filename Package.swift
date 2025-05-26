aadb382529c321a5f2e58b7ba773e865b614b885
jaykumarvcx <jayprakash.k@vcloudx.com>
2025-05-26 09:50:58 +0530

    Release 2.1.11

--- Package.swift
+++ Package.swift
@@ -15,17 +15,17 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Enx_UIKit_iOS",
-            url: "https://developer.enablex.io/downloads/ui-kit/ios/Enx_UIKit_iOS.xcframework_2.1.10.zip",
-            checksum: "6d04256af222912d9be5d642cfce67c48f78a3a80e849d1e4540e9f1158d7d94"
+            url: "https://developer.enablex.io/downloads/ui-kit/ios/Enx_UIKit_iOS.xcframework_2.1.11.zip",
+            checksum: "a884c4bd351addcb415384a90e788dbf9a1727e51a9d3729c06064135318ce69"
         ),
         .binaryTarget(
             name: "EnxRTCiOS",
-            url: "https://developer.enablex.io/downloads/iOS_SDK_3.0.19/EnxRTCiOS.xcframework_3.0.19.zip",
-            checksum: "fe1b00d967b71ca67901ed0ea40bfd30054cfae2222a1adb450a280abd2b738c"
+            url: "https://developer.enablex.io/downloads/iOS_SDK_3.0.20/EnxRTCiOS.xcframework_3.0.20.zip",
+            checksum: "e246b8f951cf222829d1b5ef7c615d4a2e37844983d5f1c06533a8219252e1e6"
         ),
         .binaryTarget(
             name: "EnablexWebRTC",
-            url: "https://developer.enablex.io/downloads/EnablexWebRTC_1.0.3.zip",
+            url: "https://developer.enablex.io/downloads/EnablexWebRTC.xcframework_1.0.4.zip",
             checksum: "6ef4e8c6f78eddbc9d4f478ea14fb111cf58199a976b15aff37779ae0fe04b6c"
         )
     ]
--- README.md
+++ README.md
@@ -14,7 +14,7 @@ To use the `Enx_UIKit_iOS` library in a SwiftPM project,
 add the following line to the dependencies in your `Package.swift` file:
 
 ```swift
-.package(url: "https://github.com/EnableX/Enx_UIKit_iOS.git", from: "2.1.10"),
+.package(url: "https://github.com/EnableX/Enx_UIKit_iOS.git", from: "2.1.11"),
 ```
 ## How to use
 
