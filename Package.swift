d8465bd8c58565334332747b0a1c6c00df05ce1f
jaykumarvcx <jayprakash.k@vcloudx.com>
2025-07-08 16:54:39 +0530

    Update libraray with VAD and chat Apis Updates

--- Package.swift
+++ Package.swift
@@ -8,25 +8,23 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
-        .library(name: "Enx_UIKit_iOS", targets: ["Enx_UIKit_iOS"]),
-        .library(name: "EnxRTCiOS", targets: ["EnxRTCiOS"]),
-        .library(name: "EnablexWebRTC", targets: ["EnablexWebRTC"])
+        .library(name: "Enx_UIKit_iOS", targets: ["Enx_UIKit_iOS","EnxRTCiOSWrapper"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/EnableX/EnxRTCiOS.git", from: "3.1.1")
     ],
     targets: [
         .binaryTarget(
             name: "Enx_UIKit_iOS",
-            url: "https://developer.enablex.io/downloads/ui-kit/ios/Enx_UIKit_iOS.xcframework_2.1.11.zip",
-            checksum: "a884c4bd351addcb415384a90e788dbf9a1727e51a9d3729c06064135318ce69"
+            url: "https://developer.enablex.io/downloads/ui-kit/ios/Enx_UIKit_iOS.xcframework_2.1.12.zip",
+            checksum: "b7fef399765544a4c6646c7f20a5bc78829acc160fe80d41e29d51cb1b2f0d10"
         ),
-        .binaryTarget(
-            name: "EnxRTCiOS",
-            url: "https://developer.enablex.io/downloads/iOS_SDK_3.1.0/EnxRTCiOS.xcframework_3.1.0.zip",
-            checksum: "9d40b991426f7010944eb2ce601573d4c3645e3cf52bbf80bb13510675afd703"
-        ),
-        .binaryTarget(
-            name: "EnablexWebRTC",
-            url: "https://developer.enablex.io/downloads/EnablexWebRTC.xcframework_2.0.0.zip",
-            checksum: "60fdf989c1fcf249a9f146cfa70654fef1b569319c5c6bd9d6d94e4edbf192a7"
+        .target(
+            name: "EnxRTCiOSWrapper",
+            dependencies: [
+                .product(name: "EnxRTCiOS", package: "EnxRTCiOS")
+            ],
+            path: "Sources/EnxRTCiOS"
         )
     ]
 )
---
+++ Sources/EnxRTCiOS/EnxRTCiOS.swift
@@ -0,0 +1,6 @@
+//
+//  EnxRTCiOS.swift
+import Foundation
+
+// Re-export EnxRTCiOS framework so users can access it as an individual framework
+//@_exported import EnxRTCiOS
