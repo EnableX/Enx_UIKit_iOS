dab4cec1fabb0f903b716980203653406f8b5b47
jaykumarvcx <jayprakash.k@vcloudx.com>
2025-07-31 12:15:48 +0530

    Update V15

--- Package.swift
+++ Package.swift
@@ -5,19 +5,19 @@ import PackageDescription
 let package = Package(
     name: "Enx_UIKit_iOS",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v15)
     ],
     products: [
         .library(name: "Enx_UIKit_iOS", targets: ["Enx_UIKit_iOS","EnxRTCiOSWrapper"])
     ],
     dependencies: [
-        .package(url: "https://github.com/EnableX/EnxRTCiOS.git", from: "3.1.1")
+        .package(url: "https://github.com/EnableX/EnxRTCiOS.git", from: "3.1.2")
     ],
     targets: [
         .binaryTarget(
             name: "Enx_UIKit_iOS",
-            url: "https://developer.enablex.io/downloads/ui-kit/ios/Enx_UIKit_iOS.xcframework_2.1.12.zip",
-            checksum: "b7fef399765544a4c6646c7f20a5bc78829acc160fe80d41e29d51cb1b2f0d10"
+            url: "https://developer.enablex.io/downloads/ui-kit/ios/Enx_UIKit_iOS.xcframework_2.2.0.zip",
+            checksum: "f50a8b430ed6a35a9ba437f20d3abdab2e0cfdfe4c32c2cb29aecc757f6f0e9a"
         ),
         .target(
             name: "EnxRTCiOSWrapper",
